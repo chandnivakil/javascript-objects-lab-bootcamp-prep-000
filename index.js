57cc7bb360ae0e0033695c86c5b0fde21e5e5035
fantastic-page-5704 <chandvakil@gmail.com>
2018-12-04 00:02:08 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,21 @@
+var recipes = {}
+
+function updateObjectWithKeyAndValue(recipes, foodItem, value) {
+  return Object.assign({}, recipes, { [foodItem]: value })
+}
+
+
+function destructivelyUpdateObjectWithKeyAndValue(recipes, foodItem, value) {
+   recipes[foodItem] = value
+   return recipes
+}
+
+function deleteFromObjectByKey(recipes, key) {
+  var newRecipe = Object.assign({}, recipes)
+  delete newRecipe[key]
+  return newRecipe
+}
+
+function destructivelyDeleteFromObjectByKey(object, key)
+delete recipes[key]
+return recipes
